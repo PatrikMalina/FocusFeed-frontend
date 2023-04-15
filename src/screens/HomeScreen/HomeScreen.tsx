6432e2dc4b91deec6c72aa543a589b75820a8e19
Patrik Malina <patrik.malina.jr@gmail.com>
2023-04-15 16:04:26 +0200

    Added token to store

--- src/screens/HomeScreen/HomeScreen.tsx
+++ src/screens/HomeScreen/HomeScreen.tsx
@@ -1,20 +1,15 @@
 import {View, Text, Button} from 'react-native';
 import React from 'react';
-import {useDispatch, useSelector} from 'react-redux';
+import {useDispatch} from 'react-redux';
 import {bindActionCreators} from 'redux';
 import * as ActionCreators from '../../state/action-creators';
-import {RootState} from '../../state';
 
 const HomeScreen = () => {
-  const token = useSelector((state: RootState) => state.token);
   const {removeToken, removeUser} = bindActionCreators(
     ActionCreators,
     useDispatch(),
   );
 
-  const getCurr = () => {
-    console.log('token curr', token);
-  };
   return (
     <View>
       <Text>Signed in!</Text>
