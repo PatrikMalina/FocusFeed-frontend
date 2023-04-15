eeed64f8b171bfc819cfba6905e217d08c755713
Patrik Malina <patrik.malina.jr@gmail.com>
2023-04-16 00:13:40 +0200

    Merge conflict resolved

--- src/screens/HomeScreen/HomeScreen.tsx
+++ src/screens/HomeScreen/HomeScreen.tsx
@@ -1,41 +1,39 @@
 import {View, Text, Button} from 'react-native';
-import React, { useEffect, useState } from 'react';
+import React, {useEffect, useState} from 'react';
 import {useDispatch, useSelector} from 'react-redux';
 import {bindActionCreators} from 'redux';
 import * as ActionCreators from '../../state/action-creators';
 import {checkMe, myPosts} from '../../services/AppService';
 import Posts from '../../components/Post';
-import { ScrollView } from 'react-native-gesture-handler';
-import store, { RootState } from '../../state/store';
-import { Post, User } from '../../util/interface';
+import {ScrollView} from 'react-native-gesture-handler';
+import {RootState} from '../../state/store';
+import {Post, User} from '../../util/interface';
 
 const HomeScreen = () => {
   const {removeToken, removeUser, setUser} = bindActionCreators(
     ActionCreators,
     useDispatch(),
   );
-  const [currentUser, setCurrentUser] = useState<User | null | undefined>()
-  const [posts, setPosts] = useState<Post[]>([])
+  const [currentUser, setCurrentUser] = useState<User | null | undefined>();
+  const [posts, setPosts] = useState<Post[]>([]);
   const user = useSelector((state: RootState) => state.user);
-  useEffect(() => {   
-    checkMe().then(
-      res => {
-        const user = res.data
-        setUser(user)
-      }
-    );
+  useEffect(() => {
+    checkMe().then(res => {
+      const user = res.data;
+      setUser(user);
+    });
   }, []);
 
   useEffect(() => {
     setCurrentUser(user);
   }, [user]);
 
-  useEffect(() =>{
-        myPosts().then(res => {
-            const postsData = res.data
-            setPosts(postsData)
-        })
-    }, [posts])
+  useEffect(() => {
+    myPosts().then(res => {
+      const postsData = res.data;
+      setPosts(postsData);
+    });
+  }, [posts]);
   return (
     <View>
       <Text>Signed in! {currentUser?.username}</Text>
@@ -48,8 +46,8 @@ const HomeScreen = () => {
         }}
       />
       <ScrollView>
-        {posts.map((post, index) =>(
-            <Posts post={post} key={index} />
+        {posts.map((post, index) => (
+          <Posts post={post} key={index} />
         ))}
       </ScrollView>
     </View>
